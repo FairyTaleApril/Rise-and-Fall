88f9ea4cff7bd4896209ab5fa026fc3484c8954e
Hazel <168259462+Hazel-ZYS@users.noreply.github.com>
2024-05-04 16:56:17 +1000

    Update Bound.py

--- RenderingPipeline/RayTracingTools/Bound.py
+++ RenderingPipeline/RayTracingTools/Bound.py
@@ -14,13 +14,16 @@ class Bound:
         self.pMin = None
         self.pMax = None
 
+    
     def bound3(self, p1, p2):
         self.pMin = np.array([min(p1[0], p2[0]), min(p1[1], p2[1]), min(p1[2], p2[2])])
         self.pMax = np.array([max(p1[0], p2[0]), max(p1[1], p2[1]), max(p1[2], p2[2])])
 
+    
     def get_centroid(self):
         return 0.5 * (self.coords_min + self.coords_max)
 
+    
     def detect_intersect(self, ray):
         inter = Intersection()
 
@@ -41,32 +44,51 @@ class Bound:
 
         return t_enter <= t_exit and t_exit >= 0
 
+    
+    def diagonal(self):
+        return self.pMax - self.pMin
 
+
+    def max_extent(self):
+        d = self.diagonal()
+        if d[0] > d[1] and d[0] > d[2]:
+            return 0
+        elif d[1] > d[2]:
+            return 1
+        else:
+            return 2
+
+    
     def intersect(self, other):
         p_min = np.maximum(self.coords_min, other.coords_min)
         p_max = np.minimum(self.coords_max, other.coords_max)
         return Bound(p_min, p_max)
 
+    
     def offset(self, point):
         o = point - self.coords_min
         if np.any(self.coords_max > self.coords_min):
             o /= self.coords_max - self.coords_min
         return o
 
+    
     def overlaps(self, other):
         x = (self.coords_max[0] >= other.coords_min[0]) and (self.coords_min[0] <= other.coords_max[0])
         y = (self.coords_max[1] >= other.coords_min[1]) and (self.coords_min[1] <= other.coords_max[1])
         z = (self.coords_max[2] >= other.coords_min[2]) and (self.coords_min[2] <= other.coords_max[2])
         return x and y and z
 
+    
     def inside(self, point):
         return np.all(point >= self.coords_min) and np.all(point <= self.coords_max)
 
+    
     def union(b1, b2):
         p_min = np.minimum(b1.pMin, b2.pMin)
         p_max = np.maximum(b1.pMax, b2.pMax)
         return Bound(p_min, p_max)
 
+    
     def union_with_point(b, p):
         p_min = np.minimum(b.pMin, p)
         p_max = np.maximum(b.pMax, p)
