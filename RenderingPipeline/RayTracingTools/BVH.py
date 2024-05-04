898cd9b031da54a30e567ebdd910e365c1075651
Hazel <168259462+Hazel-ZYS@users.noreply.github.com>
2024-05-04 16:18:21 +1000

    Update BVH.py

--- RenderingPipeline/RayTracingTools/BVH.py
+++ RenderingPipeline/RayTracingTools/BVH.py
@@ -8,11 +8,10 @@ from RenderingPipeline.RayTracingTools.Intersection import *
 class SplitMethod(Enum):
     NAIVE = 1
     SAH = 2
-    
-    def __init__(self, p, maxPrimsInNode=1, splitMethod=SplitMethod.NAIVE):
-        self.maxPrimsInNode = maxPrimsInNode
-        self.splitMethod = splitMethod
-        self.primitives = p
+
+    maxPrimsInNode = 1
+    splitMethod = NAIVE
+    primitives = []
 
 
 class BVHNode:
